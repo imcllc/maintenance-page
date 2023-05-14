b8e8ca7f63a625f8b92b20ee9d24ae44f0c14a18
Ethan McGee <ethanmcgee@iagbenefits.com>
2023-05-14 19:47:07 -0400

    v1.0.1 bug fixes

--- startup.sh
+++ startup.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
-LOGO="${LOGO:-}"
-SITE_NAME="${SITE_NAME:-We're}"
-TRY_AGAIN_TIME_UNIT="${TRY_AGAIN_TIME_UNIT:-minutes}"
-TRY_AGAIN_TIME="${TRY_AGAIN_TIME:-15}"
-STATUS="${STATUS:-}"
+if [[ -z "${LOGO}" ]]; then
+  MY_LOGO=""
+else
+  MY_LOGO="${LOGO}"
+fi
+
+if [[ -z "${SITE_NAME}" ]]; then
+  MY_SITE_NAME="We're"
+else
+  MY_SITE_NAME="${SITE_NAME}"
+fi
+
+if [[ -z "${TRY_AGAIN_TIME_UNIT}" ]]; then
+  MY_TRY_AGAIN_TIME_UNIT="minutes"
+else
+  MY_TRY_AGAIN_TIME_UNIT="${TRY_AGAIN_TIME_UNIT}"
+fi
+
+if [[ -z "${TRY_AGAIN_TIME}" ]]; then
+  MY_TRY_AGAIN_TIME="15"
+else
+  MY_TRY_AGAIN_TIME="${TRY_AGAIN_TIME}"
+fi
+
+if [[ -z "${STATUS}" ]]; then
+  MY_STATUS=""
+else
+  MY_STATUS="${STATUS}"
+fi
 
 for i in $(find /usr/share/nginx/html -type f -print)
 do
-    sed -i -e 's$LOGO$'$LOGO'$g' $i
-    sed -i -e 's$SITE_NAME$'$SITE_NAME'$g' $i
-    sed -i -e 's$TRY_AGAIN_TIME_UNIT$'$TRY_AGAIN_TIME_UNIT'$g' $i
-    sed -i -e 's$TRY_AGAIN_TIME$'$TRY_AGAIN_TIME'$g' $i
-    sed -i -e 's$STATUS$'$STATUS'$g' $i
+    sed -i -e "s#LOGO#$MY_LOGO#g" $i
+    sed -i -e "s#SITE_NAME#$MY_SITE_NAME#g" $i
+    sed -i -e "s#TRY_AGAIN_TIME_UNIT#$MY_TRY_AGAIN_TIME_UNIT#g" $i
+    sed -i -e "s#TRY_AGAIN_TIME#$MY_TRY_AGAIN_TIME#g" $i
+    sed -i -e "s#STATUS#$MY_STATUS#g" $i
 done
 
 nginx -g "daemon off;"
